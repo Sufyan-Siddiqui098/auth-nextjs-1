631e006b8f4ee3215d0df5f762b21af23333051f
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2023-09-11 14:46:36 +0500

    email verificaiton on signup is done

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+const verifyEmail =  () => {
+  const [token, setToken] = useState("");
+  const [error, setError] = useState(false);
+  const [verified, setVerified] = useState(false);
+
+  const verifyUserEmail = async () => {
+    try {
+      const res = await fetch("/api/users/verifyemail", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({token})
+      });
+      const json = await res.json();
+
+      //pop up notification
+      if (res.ok) {
+        toast.success(json.message);
+        setVerified(true)
+      } else {
+        toast.error(json.error || json.message);
+      }
+
+
+    } catch (error: any) {
+      setError(true);
+      toast.error(error.message);
+      console.log(error.message);
+    }
+  };
+
+  useEffect(()=>{
+    //url after (=) the token will be extract
+    const urlToken = window.location.search.split('=')[1];
+    setToken(urlToken || '')
+  }, [])
+
+  useEffect(() => {
+    if(token.length > 0) {
+        verifyUserEmail();
+    }
+  }, [token]);
+
+  return(
+    <div className="min-h-screen flex justify-center items-center flex-col">
+      <h1 className="text-4xl">Verify Email</h1>
+      <h2 className="bg-orange-600 p-1 my-2 rounded">{token? token: "Nothing"}</h2>
+      {verified && (
+        <div>
+          <h2 className="text-2xl">Email Verified</h2>
+          <Link className="bg-blue-600 rounded p-2 " href='/login'>Login here</Link>
+        </div>
+      )}
+      {error && (
+        <div>
+        <h2 className="text-2xl bg-red-700">Error Occured</h2>
+      </div>
+      ) }
+    </div>
+  )
+};
+
+export default verifyEmail;
